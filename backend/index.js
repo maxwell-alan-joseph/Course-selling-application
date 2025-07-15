f26c419c0201f455183f9338abb2663e844891f2
Maxwell Alan Joseph <maxwellalanjoseph@gmail.com>
2025-07-15 23:55:21 +0530

    updating index.js

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const { userRouter } = require("./routes/user");
 const { adminRouter } = require("./routes/admin");
-const { courseRouter } = require("./routes/course");
-const { userModel, adminModel, courseModel } = require("./db");
+const { MONGODB_URI, PORT } = require("./config/config");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
-app.use("/api/v1/course", courseRouter);
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+mongoose.connect(MONGODB_URI)
+    .then( () => {
+        console.log("connected to Database");
+
+        app.listen(PORT, () => {
+            console.log(`server listening on port ${PORT}`);
+        });
+    })
+    .catch( (err) => {
+        console.error(`Database connection error: ${err}`);
+        process.exit(1);
+    });
