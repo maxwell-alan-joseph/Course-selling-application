200b134c7968c395917f70e579a70409a69c409e
Maxwell Alan Joseph <maxwellalanjoseph@gmail.com>
2025-07-15 11:53:25 +0530

    uservalidation

---
+++ backend/validations/userValidation.js
@@ -0,0 +1,20 @@
+const { z } = require('zod');
+
+const userSignupSchema = z.object({
+    name: z.string().min(3, "Name must be at least 3 Characters"),
+    email: z.string().email("Invalid email format"), 
+    password: z.string().min(8, "Password must be at least 8 characters")
+});
+
+const userLoginSchema = z.object({
+    email: z.string().email("Invalid email format"),
+    password: z.string().min(8, "Password must be at least 8 characters")
+});
+
+const purchaseSchema = z.object({
+    courseId: z.string().length(12, "Invalid course ID format")
+});
+
+module.export = {
+    userSignupSchema, userLoginSchema, purchaseSchema 
+}
