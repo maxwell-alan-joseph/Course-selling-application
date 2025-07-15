e8b2e464dd1205e76d2ecb54e14ee0e9f86f5d21
Maxwell Alan Joseph <maxwellalanjoseph@gmail.com>
2025-07-15 21:55:14 +0530

    user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,48 +1,95 @@
 const { Router } = require('express');
+const bcrpyt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const { userModel, adminModel, courseModel } = require("./db");
+
+const { userModel, courseModel } = require("../models/db");
+const { userAuthMiddleware } = require("../middleware/auth");
+const { validateRequest } = require("../middleware/validation");
+const { userSignupSchema, userLoginSchema, purchaseSchema } = require("../validations/userValidation");
+const { JWT_SECRET } = require("../config/config");
 
 const userRouter = Router();
-const JWT_SECRET = "pass"
 
 
-userRouter.post("/signup", async (req, res) => {
-    const { email, password, name} = req.body;
+userRouter.post("/signup", validateRequest(userSignupSchema), async (req, res) => {
 
-   await userModel.create({
-        email, password, name
-    })
-    
-    res.json({
-        message: "You are signed up"
-    })
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await userModel.findOne({ email });
+        
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            })
+        }
+
+        const user = await userModel.create({
+            name, email, password
+        });
+
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_SECRET);
+
+        res.status(200).json({
+            message: "User created successfully",
+            token, 
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
+
 });
 
-userRouter.post("/login", async (req, res) => {
-    const { email, password} = req.body;
+userRouter.post("/login", validateRequest(userLoginSchema),  async (req, res) => {
 
-    const user = await userModel.findOne ({
-        email, password
-    })
+    try {
+        const { email, password } = req.body;
 
-    if(user) {
+        const user = await userModel.findOne({ email });
+        if(!user){
+            return res.status(401).json({
+                message: "You don't have an account try signing up first"
+            })
+        }
+
+        const isPasswordValid = await bcrpyt.compare(password, user.password);
+        if(!isPasswordValid){
+            return  res.status(401).json({
+                message: "Invalid Credentials"
+            });
+        }
+
         const token = jwt.sign({
-            id: user._id.toString()
-        }, JWT_SECRET)
-
+            id: user._id
+        }, JWT_SECRET);
+        
         res.json({
+            message: "Login successful",
             token,
-            message: "You are logged in! Welcome Back!"
-        })
-    } else {
-        res.status(300).json({
-            message: "invalid credentials"
-        })
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Server error"
+        });
     }
 });
 
-userRouter.post("/purchase", async (req, res) => {
+userRouter.post("/courses", async (req, res) => {
  //if the userId has the valid token and if the courseId from the course-db is true then await him on a buffer page for 5 seconds and redirect
  //him to the notification page saying "Congratulations on the course! Happy learning"
 
