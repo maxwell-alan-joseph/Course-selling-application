72edd71e664eb14e6cb5e037b115cbd8c73be5cb
Maxwell Alan Joseph <maxwellalanjoseph@gmail.com>
2025-07-15 11:56:54 +0530

    adminvalidation

---
+++ backend/validations/adminValidation.js
@@ -0,0 +1,16 @@
+const { z } = require ('zod');
+
+const adminSignupSchema = z.object({
+    name: z.string.min(3, "Name must be at least 3 characters"),
+    email: z.string.email("Invalid email format"), 
+    password: z.string.min(8, "Password must at least 8 characters")
+}); 
+
+const adminLoginSchema = z.object({
+    email: z.string.email("Invalid email format"),
+    password: z.string.min(8, "Password must be at least 8 characters")
+});
+
+module.exports = {
+    adminSignupSchema, adminLoginSchema
+}
