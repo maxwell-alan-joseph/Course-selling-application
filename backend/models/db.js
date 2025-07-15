4a254f94ea63cbf8c70037141aef913e88e98d41
Maxwell Alan Joseph <maxwellalanjoseph@gmail.com>
2025-07-15 11:41:48 +0530

    hashing|DB

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,20 +1,40 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.ObjectId;
 
 const userSchema = new Schema({
-    email: {type: String, unique: type},
-    password: String, 
-    name: String
-})
+    email: {type: String, unique: type, required: true, lowercase: true},
+    password: {type: String, required: true, minlength: 8}, 
+    name: { type: String, required: true},
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }],
+    timestamps: true
+});
+
+userSchema.pre('save', async (next) => {
+    if (!this.isModified('password')) 
+        return next();
+    this.password = await bcrypt.hash(this.password, 8);
+    next();
+});
 
 const adminSchema = new Schema({
-    email: {type: String, unique: type},
-    creatorId: ObjectId,
-    password: String, 
-    name: String
-})
+    email: {type: String, unique: type, required: true, lowercase: true},
+    password: {type: String, required: true, minlength: 8},
+    name: {type: String, required: true},
+    timestamps: true
+});
+
+adminSchema.pre('save', async (next) => {
+    if (!this.isModified('password'))
+        return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
 
 const courseSchema = new Schema({
     userId: ObjectId, 
